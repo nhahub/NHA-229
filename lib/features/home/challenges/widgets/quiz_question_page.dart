1524dc55e0375e98c8b5b84c3880d9e621c580f0
mirnatarek21 <mirnatarek60@gmail.com>
2025-11-29 21:22:21 +0200

    adjust screenUtil and font size un quiz screens

--- lib/features/home/challenges/widgets/quiz_question_page.dart
+++ lib/features/home/challenges/widgets/quiz_question_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mostawak/core/constants/app_colors.dart';
 
 class QuizQuestionPage extends StatelessWidget {
@@ -17,64 +18,66 @@ class QuizQuestionPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Container(
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: const Color(0xFFD9D9D9),
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              question,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 32,
-                fontFamily: "BigShoulders",
-                color: MyColors.primary,
-                fontWeight: FontWeight.w400,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 20.h),
+        child: Container(
+          padding: EdgeInsets.all(20.h),
+          decoration: BoxDecoration(
+            color: const Color(0xFFD9D9D9),
+            borderRadius: BorderRadius.circular(20.r),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                question,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24.sp, // responsive
+                  fontFamily: "BigShoulders",
+                  color: MyColors.primary,
+                  fontWeight: FontWeight.w400,
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            Column(
-              children: List.generate(answers.length, (i) {
-                bool isSelected = selectedIndex == i;
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 10),
-                  child: GestureDetector(
-                    onTap: () => onAnswerSelected(i),
-                    child: Container(
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: isSelected
-                            ? MyColors.primary
-                            : const Color(0xFFECE7E3),
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(
+              SizedBox(height: 20.h),
+              Column(
+                children: List.generate(answers.length, (i) {
+                  bool isSelected = selectedIndex == i;
+                  return Padding(
+                    padding: EdgeInsets.only(bottom: 10.h),
+                    child: GestureDetector(
+                      onTap: () => onAnswerSelected(i),
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
                           color: isSelected
-                              ? Colors.transparent
-                              : Colors.grey.shade400,
+                              ? MyColors.primary
+                              : const Color(0xFFECE7E3),
+                          borderRadius: BorderRadius.circular(10.r),
+                          border: Border.all(
+                            color: isSelected
+                                ? Colors.transparent
+                                : Colors.grey.shade400,
+                          ),
                         ),
-                      ),
-                      padding: const EdgeInsets.symmetric(vertical: 14),
-                      alignment: Alignment.center,
-                      child: Text(
-                        answers[i],
-                        style: TextStyle(
-                          color: isSelected ? Colors.white : Colors.black54,
-                          fontSize: 25,
-                          fontWeight: FontWeight.w400,
+                        padding: EdgeInsets.symmetric(vertical: 14.h),
+                        alignment: Alignment.center,
+                        child: Text(
+                          answers[i],
+                          style: TextStyle(
+                            color: isSelected ? Colors.white : Colors.black54,
+                            fontSize: 18.sp, // responsive
+                            fontWeight: FontWeight.w400,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                );
-              }),
-            ),
-          ],
+                  );
+                }),
+              ),
+            ],
+          ),
         ),
       ),
     );
